103c0af7a681b0a8295aa91700f4fd1924d74ab6
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-01-26 17:32:08 +0100

    Hash to string to improve playground performance

--- src/common.ts
+++ src/common.ts
@@ -2,16 +2,8 @@ export type Dictionary<T> = {
   [key: string]: T;
 };
 
-export type Hash = Uint8Array;
-export type AgentPubKey = Uint8Array;
+export type Hash = string;
+export type AgentPubKey = string;
 export type Signature = Uint8Array;
 
-export type CellId = [AgentPubKey, Hash];
-
-export function getAgentPubKey(cellId: CellId): AgentPubKey {
-  return cellId[0];
-}
-
-export function getDnaHash(cellId: CellId): Hash {
-  return cellId[1];
-}
+export type CellId = [Hash, AgentPubKey];
