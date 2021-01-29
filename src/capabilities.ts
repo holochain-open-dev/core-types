c40fc19280e56dfacdd891a04dd893d971911177
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-01-29 12:52:24 +0100

    Added capaccess

--- src/capabilities.ts
+++ src/capabilities.ts
@@ -1,4 +1,4 @@
-import { AgentPubKey } from './common';
+import { AgentPubKey } from "./common";
 
 export type CapSecret = string;
 
@@ -10,11 +10,16 @@ export interface CapClaim {
 
 export interface ZomeCallCapGrant {
   tag: string;
-  // TODO implement CapAccess
-  // https://github.com/holochain/holochain/blob/6b47b136bc419be6e85000a08a59f67b4c46226c/crates/zome_types/src/capability/grant.rs
-  access: string;
+  access: CapAccess;
   functions: string[];
 }
+
+export type CapAccess =
+  | "Unrestricted"
+  | {
+      Transferable: { secret: CapSecret };
+    }
+  | { Assigned: { secret: CapSecret; assignees: AgentPubKey[] } };
 
 export type CapGrant =
   | {
