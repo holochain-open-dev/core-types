2ab668e9a6381ad33624850e097cf856256ae577
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-02-24 14:36:28 +0100

    Added details

--- src/entry-details.ts
+++ src/entry-details.ts
@@ -1,9 +1,36 @@
 import { Entry } from './entry';
 import { SignedHeaderHashed } from './header';
 import { EntryDhtStatus } from './metadata';
+import { Element } from './element';
+import { ValidationStatus } from './validation';
 
 export interface EntryDetails {
   entry: Entry;
   headers: Array<SignedHeaderHashed>;
-  entry_dht_status: EntryDhtStatus
+  rejected_headers: Array<SignedHeaderHashed>;
+  deletes: Array<SignedHeaderHashed>;
+  updates: Array<SignedHeaderHashed>;
+  entry_dht_status: EntryDhtStatus;
+}
+
+export interface ElementDetails {
+  element: Element;
+  validation_status: ValidationStatus;
+  deletes: Array<SignedHeaderHashed>;
+  updates: Array<SignedHeaderHashed>;
+}
+
+export type Details =
+  | {
+      type: DetailsType.Element;
+      content: ElementDetails;
+    }
+  | {
+      type: DetailsType.Entry;
+      content: EntryDetails;
+    };
+
+export enum DetailsType {
+  Entry,
+  Element,
 }
