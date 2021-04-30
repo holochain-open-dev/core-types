2a6a27e2efeba526735747264e91ba9ee8b0de69
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-04-30 14:35:10 +0200

    Updates

--- src/validation.ts
+++ src/validation.ts
@@ -1,5 +1,15 @@
+import { AgentPubKey, Hash } from './common';
+import { Timestamp } from './timestamp';
+
 export enum ValidationStatus {
   Valid,
   Rejected,
   Abandoned,
+}
+
+export interface ValidationReceipt {
+  dht_op_hash: Hash;
+  validation_status: ValidationStatus;
+  validator: AgentPubKey;
+  when_integrated: Timestamp;
 }
