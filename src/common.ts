2dcb497b5683d4ce6634d044e42ed8a7e761b081
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-05-28 12:31:58 +0200

    Added AnyDhtHashB64

--- src/common.ts
+++ src/common.ts
@@ -8,6 +8,7 @@ export type HeaderHashB64 = HoloHashB64;
 export type DhtOpHashB64 = HoloHashB64;
 export type DnaHashB64 = HoloHashB64;
 export type AgentPubKeyB64 = HoloHashB64;
+export type AnyDhtHashB64 = HoloHashB64;
 export type Signature = Uint8Array;
 
 export type CellId = [DnaHashB64, AgentPubKeyB64];
